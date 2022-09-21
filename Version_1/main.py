60064db3a5b6a7f44ef66a36f30eca808f7dd651
zifrit <zifrit@list.ru>
2022-09-21 22:07:55 +0300

    .

--- Version_1/main.py
+++ Version_1/main.py
@@ -9,7 +9,5 @@ dp = Dispatcher(bot, loop=loop)
 if __name__ == '__main__':
     from handlerss.start_main import dp, answer
     from handlerss import admin, client
-
     client.register_command_from_bot(dp)
-
     executor.start_polling(dp, on_startup=answer, skip_updates=True)
