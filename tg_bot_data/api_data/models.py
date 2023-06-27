94aec44e7a96568064dc39aa159f73577541c366
zifrit <zifrit@list.ru>
2023-06-27 15:13:30 +0300

    created model GameCSP and updated ListGames model.

--- tg_bot_data/api_data/models.py
+++ tg_bot_data/api_data/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 
@@ -17,11 +18,30 @@ class TgUser(models.Model):
 class ListGames(models.Model):
     administrator = models.ForeignKey(to=TgUser, verbose_name='administrator', db_index=True, on_delete=models.CASCADE,
                                       related_name="my_games")
-    game_name = models.CharField(verbose_name='game name', max_length=255)
-    players = models.JSONField(verbose_name='players')
+    game_name = models.CharField(verbose_name='game name', max_length=255, db_index=True)
+    identify_game = models.IntegerField(verbose_name='id game',
+                                        validators=[
+                                             MaxValueValidator(10000),
+                                             MinValueValidator(0)
+                                         ])
 
     def __str__(self):
         return f'создал {self.administrator.__str__()} игру {self.game_name}'
 
     class Meta:
         db_table = 'TG_list_games'
+
+
+class GameCSP(models.Model):
+    list_games = models.OneToOneField(to=ListGames, verbose_name='connection to list', on_delete=models.CASCADE)
+    number_players = models.IntegerField(verbose_name='number of players', default=2,
+                                         validators=[
+                                             MaxValueValidator(2),
+                                             MinValueValidator(0)
+                                         ])
+    in_game = models.IntegerField(verbose_name='players in game',
+                                  validators=[
+                                      MaxValueValidator(2),
+                                      MinValueValidator(0)
+                                  ])
+    players = models.JSONField(verbose_name='players')
